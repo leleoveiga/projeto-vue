611d5975bed6783feb7c80961a60c2f75d07dae0
Leonardo Veiga <leonardo.porfirio@ccc.ufcg.edu.br>
2020-11-25 17:13:36 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+	<div id="app">
+		<Header />
+		<Menu />
+		<Content />
+		<Footer />
+	</div>
+</template>
+
+<script>
+import Header from "./components/template/Header";
+import Menu from "@/components/template/Menu";
+import Content from "@/components/template/Content";
+import Footer from "@/components/template/Footer";
+
+export default {
+	name: "App",
+	components: { Header, Menu, Content, Footer },
+};
+</script>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	/* margin-top: 60px; */
+	height: 100vh;
+	display: grid;
+	grid-template-rows: 60px 1fr 40px;
+	grid-template-columns: 300px 1fr;
+	grid-template-areas:
+		"header header"
+		"menu content"
+		"menu footer";
+}
+
+body {
+	margin: 0px;
+}
+</style>
---
+++ src/components/template/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+	<footer class="footer">
+		<span>Copyright <strong>Leonardo Veiga</strong></span>
+	</footer>
+</template>
+
+<script>
+export default {
+	name: "Footer",
+};
+</script>
+
+<style>
+.footer {
+	grid-area: footer;
+	background-color: #ddd;
+	color: #333;
+
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	padding-right: 10px;
+}
+</style>
